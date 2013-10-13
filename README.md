6adead891fcbefea81ee0a36d9501676ed77e564
Stuart Starr <stuart@stueynet.com>
2013-10-13 14:11:45 -0400

    new version number

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 stueynet-network-footer
 =======================
-~Current Version:0.2~
+~Current Version:0.4~
 
 This plugin will have little use for you. I use it to display my site network at the bottom of my sites.
--- stueynet.php
+++ stueynet.php
@@ -4,34 +4,43 @@
  * Plugin Name: Stueynet Footer
  * Plugin URI: http://stuey.net
  * Description: Stueynet Footer on the bottom
- * Version: 0.2
+ * Version: 0.4
  * Author: Stuart Starr
- * Author URI: httpo://stuey.net
+ * Author URI: http://stuey.net
 */
  
  
  
 // include updater
  
-include_once('updater.php');
- 
- if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
-    $config = array(
-        'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-        'proper_folder_name' => 'stueynet-network-footer', // this is the name of the folder your plugin lives in
-        'api_url' => 'https://api.github.com/repos/stueynet/stueynet-network-footer', // the github API url of your github repo
-        'raw_url' => 'https://raw.github.com/username/stueynet-network-footer/master', // the github raw url of your github repo
-        'github_url' => 'https://github.com/stueynet/stueynet-network-footer', // the github url of your github repo
-        'zip_url' => 'https://github.com/stueynet/stueynet-network-footer/zipball/master', // the zip url of the github repo
-        'sslverify' => true, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-        'requires' => '3.0', // which version of WordPress does your plugin require?
-        'tested' => '3.3', // which version of WordPress is your plugin tested up to?
-        'readme' => 'README.md', // which file to use as the readme for the version number
-        'access_token' => '', // Access private repositories by authorizing under Appearance > Github Updates when this example plugin is installed
-    );
-    new WP_GitHub_Updater($config);
-}
- 
+add_action( 'init', 'stueynet_update_init' );
+function stueynet_update_init() {
+
+	include_once 'updater.php';
+
+	define( 'WP_GITHUB_FORCE_UPDATE', true );
+
+	if ( is_admin() ) { // note the use of is_admin() to double check that this is happening in the admin
+
+		$config = array(
+			'slug' => plugin_basename( __FILE__ ),
+			'proper_folder_name' => 'stueynet-network-footer',
+			'api_url' => 'https://api.github.com/repos/stueynet/stueynet-network-footer',
+			'raw_url' => 'https://raw.github.com/stueynet/stueynet-network-footer/master',
+			'github_url' => 'https://github.com/stueynet/stueynet-network-footer',
+			'zip_url' => 'https://github.com/stueynet/stueynet-network-footer/archive/master.zip',
+			'sslverify' => true,
+			'requires' => '3.0',
+			'tested' => '3.3',
+			'readme' => 'README.md',
+			'access_token' => '',
+		);
+
+		new WP_GitHub_Updater( $config );
+
+	}
+
+} 
  
  
 function stueynet_footer() {
